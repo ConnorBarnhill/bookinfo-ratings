a3e9c044d457893b538c72172d6fb9187f71c8c0
Connor Barnhill <connor.barnhill@plexogen.com>
2018-11-27 05:54:00 -0600

    remove build script

--- build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker build -t "istio/examples-bookinfo-ratings-v1:${VERSION}" -t istio/examples-bookinfo-ratings-v1:latest --build-arg service_version=v1 .
-docker build -t "istio/examples-bookinfo-ratings-v2:${VERSION}" -t istio/examples-bookinfo-ratings-v2:latest --build-arg service_version=v2 .
